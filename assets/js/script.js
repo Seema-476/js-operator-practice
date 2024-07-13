ba94ac2e79a8bd7d31459a41894e8d0a9e38d14a
Seema-476 <simmybishnoi29085@gmail.com>
2024-07-13 09:59:18 +0530

    js-operator-practice

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+// addition + 
+var a = 5;
+var b = 4;
+var x = a + b;
+
+console.log(x)
+// subtract -
+const y = 10;
+const z = 4;
+const c = y - z;
+console.log(c)
+// multiply *
+let two = 2;
+let five = 5;
+let multi = two * five;
+console.log(multi)
+// power **
+var power1 = 3;
+var power2 = 5;
+var power = power1 ** power2;
+console.log(power)
+// division (/)
+var division = 6;
+var divisionvalue = 3;
+var divide = division / divisionvalue;
+console.log(divide)
+// modules/reminder %
+var division = 6;
+var divisionvalue = 3;
+var divide = division % divisionvalue;
+console.log(divide)
+// increment
+var division = 6;
+var divisionvalue = 3;
+var divide = division + divisionvalue+division;
+console.log(divide)
+// decrement
+var division = 6;
+var divisionvalue = 3;
+var divide = division - divisionvalue - division;
+console.log(divide)
